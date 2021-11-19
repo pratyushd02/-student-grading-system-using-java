9ccdfb2ad0d6ad17d530605af3a1fac0fcc93b57
pratyushd02 <79018880+pratyushd02@users.noreply.github.com>
2021-11-19 14:57:06 +0530

    Add files via upload

---
+++ Java_miniproject/src/java_miniproject/Java_miniproject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_miniproject;
+
+/**
+ *
+ * @author Pratyush
+ */
+public class Java_miniproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login l = new login();
+        l.setVisible(true);
+    }
+    
+}
